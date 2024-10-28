488107a1a35a72f4a4938098704b9e1891b9be9a
ClassmateAda <classmateada@gmail.com>
2024-10-29 07:16:52 +1100

    Initialise project (unable to start due to data source not configured)

---
+++ server/src/main/java/com/adamu/leura/server/LeuraServerApplication.java
@@ -0,0 +1,11 @@
+package com.adamu.leura.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeuraServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeuraServerApplication.class, args);
+    }
+}
---
+++ worker/src/main/java/com/adamu/leura/worker/LeuraWorkerApplication.java
@@ -0,0 +1,11 @@
+package com.adamu.leura.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeuraWorkerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeuraWorkerApplication.class, args);
+    }
+}
